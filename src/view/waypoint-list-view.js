1178308035256c1711124feb3afb9fe9f2cd9e21
unknown <sergeyl9pin@gmail.com>
2022-05-01 08:16:08 +0300

    changed blockquotes

--- src/view/waypoint-list-view.js
+++ src/view/waypoint-list-view.js
@@ -1,6 +1,6 @@
 import {createElement} from '../render.js';
 
-const createWaypointListTemplate = () => (`<ul class="trip-events__list"></ul>`);
+const createWaypointListTemplate = () => ('<ul class="trip-events__list"></ul>');
 
 export default class WaypointListView {
   getTemplate() {
