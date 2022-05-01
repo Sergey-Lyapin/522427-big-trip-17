d16dfe9ba6d74678d0cb5f9f2524f8024768503a
unknown <sergeyl9pin@gmail.com>
2022-05-01 07:59:12 +0300

    made some changes

--- src/presenter/waypoint-list-presenter.js
+++ src/presenter/waypoint-list-presenter.js
@@ -4,17 +4,13 @@ import WaypointView from '../view/waypoint-view.js';
 import {render} from '../render.js';
 
 export default class WaypointListPresenter {
-    waypointListComponent = new WaypointListView();
-  
-    init = (waypointListContainer) => {
-
-      this.waypointListContainer = waypointListContainer;
-  
-      render(this.waypointListComponent, this.waypointListContainer);
-      render(new EditFormView(), this.waypointListComponent.getElement());
-  
-      for (let i = 0; i < 3; i++) {
-        render(new WaypointView(), this.waypointListComponent.getElement());
-      }
-    };
-  }
+  waypointListComponent = new WaypointListView();
+  init = (waypointListContainer) => {
+    this.waypointListContainer = waypointListContainer;
+    render(this.waypointListComponent, this.waypointListContainer);
+    render(new EditFormView(), this.waypointListComponent.getElement());
+    for (let i = 0; i < 3; i++) {
+      render(new WaypointView(), this.waypointListComponent.getElement());
+    }
+  };
+}
