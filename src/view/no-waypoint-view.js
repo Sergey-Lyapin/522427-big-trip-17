226d87e67f0c339155fe45f42e86b27522c3073b
unknown <sergeyl9pin@gmail.com>
2022-05-22 00:26:02 +0300

    made changes

--- src/view/no-waypoint-view.js
+++ src/view/no-waypoint-view.js
@@ -1,23 +1,19 @@
 import {createElement} from '../render.js';
 
-const createNoWaypointTemplate = () => (`<p class="trip-events__msg">Click New Event to create your first point</p>`);
+const createNoWaypointTemplate = () => ('<p class="trip-events__msg">Click New Event to create your first point</p>');
 
 export default class NoWaypointView {
-    #element = null;
-  
-    get template() {
-      return createNoWaypointTemplate();
-    }
-  
-    get element() {
-      if (!this.#element) {
-        this.#element = createElement(this.template);
-      }
-  
-      return this.#element;
-    }
-  
-    removeElement() {
-      this.#element = null;
-    }
+  #element = null;
+  get template() {
+    return createNoWaypointTemplate();
   }
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
+    }
+    return this.#element;
+  }
+  removeElement() {
+    this.#element = null;
+  }
+}
