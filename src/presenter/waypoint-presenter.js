078ecfdaf641670bb28e58049e662458d8480735
unknown <sergeyl9pin@gmail.com>
2022-06-13 18:52:46 +0300

    made changes

--- src/presenter/waypoint-presenter.js
+++ src/presenter/waypoint-presenter.js
@@ -12,7 +12,7 @@ export default class WaypointPresenter {
     this.#waypointsContainer = waypointsContainer;
     this.#changeData = changeData;
   }
-    
+  
   init = (waypoint) => {
     this.#waypoint = waypoint;
 
